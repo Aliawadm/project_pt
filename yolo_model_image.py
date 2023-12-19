87de40c12122a35c31a54c53873b9f6f3fbb6a92
Aliawadm <122026602+Aliawadm@users.noreply.github.com>
2023-12-19 21:08:49 +0300

    ali

--- yolo_model_image.py
+++ yolo_model_image.py
@@ -1,9 +1,5 @@
 # yolo_model.py
 from ultralytics import YOLO
-import cv2
-import os
-import re
-import pandas
 
 def detect_objects_image(image):
     model = YOLO('best.pt')  # load a custom model
